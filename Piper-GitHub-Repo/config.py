c6ad146a54d684d8ce0254549655a78c34983f10
keeyongtan <69494496+keeyongtan@users.noreply.github.com>
2020-08-17 08:51:16 +0000

    Add files via upload

---
+++ Piper-GitHub-Repo/config.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132417743526650312@ecstestdrive.com',
+    'ecs_secret_key' : '5qKLeoMmqMW3LTwhVyKHY447ElkTaTlLuq7gFhlW',
+    'ecs_bucket_name' : 'photo-album'
+}
